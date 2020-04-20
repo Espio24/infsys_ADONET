80c542c27b8efc5047c0636b15b5f9792bc91dec
Espio24 <63601588+Espio24@users.noreply.github.com>
2020-04-20 16:06:22 +0500

    Connection

---
+++ infsys/infsys/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace infsys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            try
+            {
+                // Открываем подключение
+                connection.Open();
+                Console.WriteLine("Подключение открыто");
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                // закрываем подключение
+                connection.Close();
+                Console.WriteLine("Подключение закрыто...");
+            }
+
+            Console.Read();
+       
+              }
+    }
+}
